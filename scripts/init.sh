575ee2affd5a17254b698e8bd9e50286c017696f
HORII Keima <holysugar@gmail.com>
2019-01-10 14:38:35 +0900

    Update init script

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,29 +1,27 @@
 #!/bin/sh
 
-origdir=$(cd $(dirname $0)/.. ; pwd)
+origdir=$( cd $(dirname $0)/.. ; pwd )
+dotfiles=$( echo dotfiles/{*,.*} )
 
 cd ~
 
-for i in \
-  .gitconfig \
-  .gitignore_global \
-  .gvimrc \
-  .screenrc \
-  .vim \
-  .vimrc \
-  .zshenv \
-  .zshrc \
-  rails.watchr \
-; do
-  ln -s $origdir/$i ~/
+for i in ${dotfiles}; do
+  # ignore directory entries
+  # ${i:-1} とすると意味が変わってしまうので ${i: -1} のスペースは必須
+  if [ "${i: -1}" = "." ]; then
+    continue
+  fi
+  ln -s $origdir/$i
 done
 
 # .config/brewfile
 mkdir -p ~/.config/brewfile 2>/dev/null
 (
   cd ~/.config/brewfile
-  ln -s $origdir/dotconfig/brewfile/Brewfile .
+  ln -s $origdir/dotconfig/brewfile/Brewfile
 )
+
+brew file install
 
 # chsh
 sudo dscl . -create /Users/$USER UserShell /bin/zsh
